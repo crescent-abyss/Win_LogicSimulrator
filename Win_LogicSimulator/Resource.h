e59ccbbd4074bf2b8d7bc2deb850113c52344b18
lkbid2 <lkbid2@naver.com>
2016-05-29 21:49:56 +0900

    비트맵

--- Win_LogicSimulator/Resource.h
+++ Win_LogicSimulator/Resource.h
@@ -6,13 +6,18 @@
 #define IDR_MAINFRAME                   128
 #define IDR_Win_LogicSimulaTYPE         130
 #define IDD_LISTDLG                     310
-#define IDB_BITMAP1                     317
-#define IDB_BITMAP12                    318
-#define IDB_BITMAP13                    319
-#define IDB_BITMAP14                    320
-#define IDB_BITMAP2                     327
-#define IDB_BITMAP15                    328
-#define IDB_BITMAP3                     337
+#define IDB_BITMAP1                     317		//and게이트0
+#define IDB_BITMAP12                    318		//and게이트1
+#define IDB_BITMAP13                    319		//and게이트2
+#define IDB_BITMAP14                    320		//and게이트3
+#define IDB_BITMAP2                     327		//or게이트0
+#define IDB_BITMAP16                    328		//or게이트1
+#define IDB_BITMAP17                    329		//or게이트2
+#define IDB_BITMAP18                    330
+#define IDB_BITMAP3                     337		//not게이트 0
+#define IDB_BITMAP19                    338
+#define IDB_BITMAP20                    339
+#define IDB_BITMAP21                    340
 #define IDB_BITMAP4                     347
 #define IDB_BITMAP5                     357
 #define IDB_BITMAP6                     367
@@ -21,12 +26,18 @@
 #define IDB_BITMAP9                     397
 #define IDB_BITMAP10                    407
 #define IDB_BITMAP11                    417
-
+#define IDB_BITMAP22                    427
+#define IDB_BITMAP23                    437
+#define IDB_BITMAP24                    447
+#define IDB_BITMAP25                    457
+#define IDB_BITMAP26                    467
+#define IDB_BITMAP27                    477
+#define IDB_BITMAP15                    487
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        329
+#define _APS_NEXT_RESOURCE_VALUE        335
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           311
