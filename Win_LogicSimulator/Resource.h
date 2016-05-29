93c7ad02037c74f3ee3e194f3113a72c4b38ccec
lkbid2 <lkbid2@naver.com>
2016-05-29 22:12:21 +0900

    res 추가.

--- Win_LogicSimulator/Resource.h
+++ Win_LogicSimulator/Resource.h
@@ -6,17 +6,14 @@
 #define IDR_MAINFRAME                   128
 #define IDR_Win_LogicSimulaTYPE         130
 #define IDD_LISTDLG                     310
-
 #define IDB_BITMAP1                     317
 #define IDB_BITMAP12                    318
 #define IDB_BITMAP13                    319
 #define IDB_BITMAP14                    320
-
 #define IDB_BITMAP2                     327
 #define IDB_BITMAP16                    328
 #define IDB_BITMAP17                    329
 #define IDB_BITMAP18                    330
-
 #define IDB_BITMAP3                     337
 #define IDB_BITMAP19                    338
 #define IDB_BITMAP20                    339
@@ -28,7 +25,15 @@
 #define IDB_BITMAP24                    350
 
 #define IDB_BITMAP5                     357
+#define IDB_BITMAP28                    358
+#define IDB_BITMAP29                    359
+#define IDB_BITMAP30                    360
+
 #define IDB_BITMAP6                     367
+#define IDB_BITMAP25                    368
+#define IDB_BITMAP26                    369
+#define IDB_BITMAP27                    370
+
 #define IDB_BITMAP7                     377
 #define IDB_BITMAP8                     387
 #define IDB_BITMAP9                     397
@@ -36,11 +41,18 @@
 #define IDB_BITMAP11                    417
 #define IDB_BITMAP15                    487
 
+//IDB_BITMAP25            BITMAP                  "res\\XOR1.bmp"
+//IDB_BITMAP26            BITMAP                  "res\\XOR2.bmp"
+//IDB_BITMAP27            BITMAP                  "res\\XOR3.bmp"
+//IDB_BITMAP28            BITMAP                  "res\\NOR1.bmp"
+//IDB_BITMAP29            BITMAP                  "res\\NOR2.bmp"
+//IDB_BITMAP30            BITMAP                  "res\\NOR3.bmp"
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        342
+#define _APS_NEXT_RESOURCE_VALUE        352
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           311
